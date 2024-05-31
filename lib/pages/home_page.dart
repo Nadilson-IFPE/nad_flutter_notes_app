8c46faaa0cf0603dd4bc9158cb743ad63cab82ea
Nadilson <nadilson@protonmail.com>
2024-05-31 01:32:53 -0300

    Notes listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:nad_flutter_notes_app/dialogs/note_dialog.dart';
+import 'package:nad_flutter_notes_app/services/firestore_service.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -9,6 +11,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  FirestoreService firestoreSvc = FirestoreService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +23,40 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () => NoteDialog.openNoteDialog(context),
         child: const Icon(Icons.add),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreSvc.listNotas(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List listaDeNotas = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: listaDeNotas.length,
+              itemBuilder: (context, idx) {
+                DocumentSnapshot documentSnapshot = listaDeNotas[idx];
+                String notaID = documentSnapshot.id;
+                Map<String, dynamic> dados =
+                    documentSnapshot.data() as Map<String, dynamic>;
+                String textoNota = dados['nota'];
+                return ListTile(
+                  title: Text(textoNota),
+                );
+              },
+            );
+          } else {
+            return const Align(
+              alignment: Alignment.topCenter,
+              child: Text(
+                'Nenhuma nota cadastrada, ainda...',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+        },
       ),
     );
   }
